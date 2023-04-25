9aaf1e97c71bf496e46974624e5bfc31879d34e6
Gonzalo <gonzaloallenperez444@gmail.com>
2023-04-24 23:15:05 -0700

    just making a basic server

--- server/app.js
+++ server/app.js
@@ -3,6 +3,14 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+
+//Gonzalo trying out server
+const http = require('http');
+const hostName = "127.0.0.1";
+const port = 8000;
+
+//end server
+
 const usersRouter = require('./routes/users');
 
 const app = express();
@@ -15,10 +23,32 @@ app.use('/users', usersRouter);
 
 dotenv.config();
 
+
+//NO DB YET
+/*
 mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true }).then(() => {
   console.log('Connected to MongoDB database');
 });
+*/
+
+
+
+
+const server = http.createServer(function (req,res){
+
+  //write header
+  res.writeHead(200,{"Content-Type": "text/plain"});
+  //send response body
+  res.end("Hello World\n")
+});
+
+
+
+//prints a log once the server starts listening
+
+server.listen(port,hostName,function() {console.log(`Server running on http://${hostName}:${port}`)});
+
 
 module.exports = app;
