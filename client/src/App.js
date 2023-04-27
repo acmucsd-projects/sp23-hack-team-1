0dc079470f8e76ef552013eb794ccfa0f938506a
Joshua Chen <joshchen43@gmail.com>
2023-04-27 16:41:10 -0700

    added,"Josh was here :0"

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload. Gonzalo was here 0_0
         </p>
+        <p>
+        Joshua was here :0
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
