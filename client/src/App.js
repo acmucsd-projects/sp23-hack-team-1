b1e7f36dd1196b9be893bad287daf5af2cfd8b32
gregoryweber <gregory1@weberdao.org>
2023-04-28 10:50:58 -0700

    first test of fetching the api

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import WordCell from "./components/Word Cell/WordCell";
 
 import "./App.css";
+
+/**
+ * Acquires a board of cards via the api.
+ * @param {import("react").SetStateAction} setCards
+ * setCards: a function of which to set the state of with the cards obtained from the api.
+ * @returns {void}
+ */
+async function getCards(setCards) {
+    const response = await fetch("http://127.0.0.1:8001/api/newboard");
+    const jsonData = await response.json();
+    console.log(jsonData);
+    setCards(jsonData.words.map((wordObj) => wordObj.word));
+}
 
 function App() {
     const [cells, setCells] = useState([
@@ -16,6 +29,10 @@ function App() {
         "test8",
         "test9",
     ]);
+
+    useEffect(() => {
+        getCards(setCells);
+    }, []);
 
     return (
         <div className="App">
