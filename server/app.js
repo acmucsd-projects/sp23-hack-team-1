17abf38bbec21b005f22826c55232c6eba05b0cc
Gonzalo <gonzaloallenperez444@gmail.com>
2023-05-08 20:05:49 -0700

    cors, we should probably implement .env files

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(
     cors({
-        origin: "http://localhost:8001",
+        origin: "http://localhost:3001",
         credentials: true,
     })
 );
