25f75886758e597e182dd92c0794ecc74c233e78
gregoryweber <gregory1@weberdao.org>
2023-05-20 11:40:08 -0700

    made scrolling work with switch component

--- client/src/components/Switch/Switch.js
+++ client/src/components/Switch/Switch.js
@@ -1,18 +1,16 @@
-import React from "react"
-import "./Switch.css"
+import React from "react";
+import "./Switch.css";
 
-
-const Switch = ({ isPlayerFunction,isPlayer }) => {
-    function onClick(){
-        isPlayerFunction(!isPlayer)
+const Switch = ({ isPlayerFunction, isPlayer }) => {
+    function onClick() {
+        isPlayerFunction(!isPlayer);
     }
     return (
-        <label className= "switch">
-            <input type = "checkbox" onChange={onClick}/> 
-            <span className = "slider" />
+        <label className="switch">
+            <input type="checkbox" onChange={onClick} />
+            <span className="slider" />
         </label>
-
-    ); 
+    );
 };
 
 export default Switch;
