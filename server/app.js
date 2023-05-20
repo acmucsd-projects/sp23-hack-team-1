19a7f8799fc3920573b41ace318854d96fa75afa
gregoryweber <gregory1@weberdao.org>
2023-05-20 10:10:15 -0700

    fixed again

--- server/app.js
+++ server/app.js
@@ -27,18 +27,11 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(
-    cors({
-        origin: "http://localhost:3000",
-        credentials: true,
-    })
-);
-
 // NOTE: just for development purposes. some reason there is conflict
 // on mac and no conflict on windows ...
 app.use(
     cors({
-        origin: "http://localhost:3001",
+        origin: ["http://localhost:3000", "http://localhost:3001"],
         credentials: true,
     })
 );
