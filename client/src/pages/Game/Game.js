a4d9eed2c898098cb977d675ad9f9a758fb81aad
Joshua Chen <joshchen43@gmail.com>
2023-05-25 16:01:54 -0700

    Added box to keep track of player guesses`

--- client/src/pages/Game/Game.js
+++ client/src/pages/Game/Game.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import WordCell from "../../components/Word Cell/WordCell";
 import "./Game.css";
 import Switch from "../../components/Switch/Switch"
+import Counter from "../../components/Counter/Counter"
+
 
 
 
@@ -30,26 +32,34 @@ async function getCards(setCards) {
 function Game() {
     const [cells, setCells] = useState([]);
     const [isPlayerView, setIsPlayerView] = useState(false);
+    const[playerGuess, setPlayerGuess] = useState(0);
 
     useEffect(() => {
         getCards(setCells);
     }, []);
 
     async function handleCardClick(index, ) {
+        if(playerGuess === 0){return} //doesn't change color because no more guesses 
         const response = await fetch(`http://127.0.0.1:8001/api/guess?index=${index}`)
         const jsonData = await response.json();
         console.log(jsonData);
         if (jsonData === "blue has won! play again?" || jsonData === "red has won! play again?") {
             setCells([]);
+
         }
         else {
-            setCells(jsonData.words);
+            setPlayerGuess( playerGuess => playerGuess - 1)
+            setCells(jsonData.words);  
+            console.log(playerGuess)
+
         }
+
     }
     
     return (
         <div className="Game">
             <Switch isPlayerFunction={setIsPlayerView} isPlayer={isPlayerView}/>
+            <Counter guessAmount={playerGuess} />
             {cells.map((cell, index) => (
                 <WordCell cell={cell} key={`${cell.word}-${index}`} isPlayer={isPlayerView} handleCardClick={handleCardClick}/> 
             ))}
