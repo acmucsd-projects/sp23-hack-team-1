339e58cce5f3079d113a4f1348232fa72c25e545
gregoryweber <gregory1@weberdao.org>
2023-05-25 16:05:45 -0700

    added spy input

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,7 @@ import Game from "./pages/Game/Game";
 import StartMenu from "./pages/StartMenu/StartMenu";
 
 function App() {
-    return (
-        <Game />
-
-
-    )
+    return <Game />;
 }
 
-export default App
+export default App;
