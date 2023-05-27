2f45a58c1e2151d4b7942ae93940b470c41d2aa1
gregoryweber <gregory1@weberdao.org>
2023-05-27 12:05:34 -0700

    switched to run game (oops)

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import Game from "./pages/Game/Game";
 import StartMenu from "./pages/StartMenu/StartMenu";
 
 function App() {
-    return <StartMenu />;
+    return <Game />;
 }
 
 export default App;
